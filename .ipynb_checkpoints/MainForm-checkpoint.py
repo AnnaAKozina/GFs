161f04c9a805675e3c5af4a5e91680fdab70f1d3
Anna <mr.stofel@gmail.com>
2020-04-19 19:52:09 +0300

    new calcs

--- .ipynb_checkpoints/MainForm-checkpoint.py
+++ .ipynb_checkpoints/MainForm-checkpoint.py
@@ -9,11 +9,18 @@ warnings.filterwarnings("ignore")
 engine_to = sqlalchemy.create_engine("mssql+pymssql://gleb-pwc:changeme@srv-ax-test/pwc")
 
 def calculate_price(df, no_competitors=False, deficit=False, pickup=False, trades_type=1, **kwargs):
+    if df.shape[0] == 0:
+        return pd.DataFrame(columns=['НоменклатураКод', 'Номенклатура', 'УчетнаяЦена', 'РекомендованнаяЦена'])
+        
     dopobjem = kwargs.get('dopobjem')
     artfruit = kwargs.get('artfruit') 
     heatseal = kwargs.get('heatseal')
     superpereborka = kwargs.get('superpereborka')
     sku = kwargs.get('sku')
+    toolow = kwargs.get('toolow')
+    toohigh = kwargs.get('toohigh')
+    toolowvalue = kwargs.get('toolowvalue')
+    toohighvalue = kwargs.get('toohighvalue')
     
     skidka_dopobjem = 0
     
@@ -26,6 +33,17 @@ def calculate_price(df, no_competitors=False, deficit=False, pickup=False, trade
             if 1.4*df['НедельныйОбъемПродаж'].values[0] < dopobjem:
                 (p_uch, p1, p2, v1) = df[['УчетнаяЦенаКГ', 'СредняяЦенаКГ', 'РекомендованнаяЦена', 'НедельныйОбъемПродаж']].values[0]
                 skidka_dopobjem = (p1 - p_uch) * (v1 + dopobjem) * 0.5 / p1 / dopobjem + p_uch/p1 + 1
+                
+        # Если в интерфейсе системы для выбранного SKU пользователь проставляет галочку в поле «Слишком низкая учетная цена», 
+        # необходимо использовать значение из поля <РазмерСкидки> источника №8, соответствующему значениям «Дефицит» 
+        # и «Дефицит на 1-2 недели» в первых двух столбцах таблицы, соответственно
+        # Если в интерфейсе системы для выбранного SKU пользователь проставляет галочку в поле «Слишком высокая учетная цена», 
+        # необходимо использовать значение из поля <РазмерСкидки> источника №8, соответствующему значениям «Оверсток» и 
+        # «Оверсток от 150% до 200% стандартного запаса» в первых двух столбцах таблицы, соответственно 
+        if toolow:
+            df['ИзменениеЦены'] = toolowvalue
+        if toohigh:
+            df['ИзменениеЦены'] = toohighvalue
             
     if not no_competitors:
         #Первая группа наценок
@@ -38,6 +56,26 @@ def calculate_price(df, no_competitors=False, deficit=False, pickup=False, trade
 
         #Вторая группа наценок - применяются к Новой Цене (после фиксированной наценки за логистику и упаковку)
         df['РекомендованнаяЦена'] = df['РекомендованнаяЦена'] * df['Экспедирование']
+        df['РекомендованнаяЦена'] = df['РекомендованнаяЦена'] * df['Штраф']
+        #Дефицит-Оверсток
+        #Скидка по оверстоку применяется только для клиентов <КонтрагентКод>, для которых разрешается ее применение
+        if not df.iloc[0]['СкидкаПриОверстоке']:
+            df.loc[df['ИзменениеЦены']<0,'ИзменениеЦены'] = 0
+        df['РекомендованнаяЦена'] = df['РекомендованнаяЦена'] * (1 + df['ИзменениеЦены'])
+        
+        #Дефицитные торги
+        if deficit:
+            df['РекомендованнаяЦена'] = df.apply(lambda x: x['РекомендованнаяЦена'] if pd.isnull(x['ЦенаТекущихТоргов'])
+                                                                                     else x['ЦенаТекущихТоргов'], axis=1)
+            
+        # В самом конце Бонусы и премии
+        df['РекомендованнаяЦена'] = df['РекомендованнаяЦена']*df['Бонус']
+        
+        # Если рекомендованная цена продажи с учетом скидки за оверсток ниже учетной цены, рекомендованная цена = учетной цене
+        if df.iloc[0]['СкидкаПриОверстоке']:
+            df.loc[(df['ИзменениеЦены']<0)&(df['РекомендованнаяЦена']<df['УчетнаяЦена']),'РекомендованнаяЦена'] = \
+            df.loc[(df['ИзменениеЦены']<0)&(df['РекомендованнаяЦена']<df['УчетнаяЦена']),'УчетнаяЦена']
+        
     else:
         df['РекомендованнаяЦена'] = df['РекомендованнаяЦена_']
 
@@ -51,8 +89,19 @@ class MainForm:
         self.start = start
         self.finish = finish
         self.df = pd.DataFrame()
+        self.toolowvalue = pd.read_sql("""select [Изменение_рекомендованной_цены_продажи] 
+from skidki_overstok
+where [Описание_ситуации_на_рынке]='Дефицит'
+and [Описание_состояния_стока_ГФ]='Дефицит на 1-2 недели'""", engine_to).iloc[0]['Изменение_рекомендованной_цены_продажи']
+        self.toohighvalue = pd.read_sql("""select [Изменение_рекомендованной_цены_продажи] 
+from skidki_overstok
+where [Описание_ситуации_на_рынке]='Оверсток'
+and [Описание_состояния_стока_ГФ]='Оверсток от одного лишнего дня до 200% стандартного запаса'""", engine_to).iloc[0]['Изменение_рекомендованной_цены_продажи']
         
     def get_data(self):
+        # !!!!! Узнать !!!!!
+        VAT = 0.2
+        
         # С учетной ценой
         sku_df = pd.read_sql(f"""select convert(int, s.НоменклатураКод) НоменклатураКод, 
         s.Номенклатура,
@@ -67,6 +116,7 @@ class MainForm:
         cs.НоменклатураКод in {tuple(sku_df['НоменклатураКод'])}"""
         
         self.df = pd.read_sql(f"""select 
+        s.ЦеноваяГруппаКод,
         cs.НоменклатураКод, 
         cs.МаксимальнаяЦенаПродажиКГ*s.ЕдиницаХраненияОстатковВес РекомендованнаяЦена_, --без конкурентов
         (1 + c.Экспедирование * c.Экспедирование_W) Экспедирование,
@@ -76,10 +126,17 @@ class MainForm:
         НедельнаяСуммаПродаж, НедельныйОбъемПродаж,
         c.[ЗатратыЛогистика, руб/кг] * s.ЕдиницаХраненияОстатковВес ЗатратыЛогистика,
         s.ЕдиницаХраненияОстатковВес,
-        s.ЗатратыСклад
+        s.ЗатратыСклад,
+        1.05 * cs.ЦенаТекущихТорговЗаКг * s.ЕдиницаХраненияОстатковВес ЦенаТекущихТоргов,
+        (1 + (({1 - VAT}) * c.Бонус + c.Премия) * c.Бонус_W) Бонус,
+        (1 + c.Штраф) Штраф,
+        dov.СостояниеСтокаГФ, dov.Ситуация_на_рынке, dov.ИзменениеЦены,
+        c.СкидкаПриОверстоке
         from ClientSKU cs 
         join Clients c on c.КонтрагентКод=cs.КонтрагентКод
         left join SKU s on s.НоменклатураКод=cs.НоменклатураКод
+        left join DeficitOverstock dov on dov.ЦеноваяГруппаКод=s.ЦеноваяГруппаКод
+        and dov.Дата='{self.start}'
         left join Profitability p on p.НоменклатураКод = cs.НоменклатураКод  and
         p.Год=year('{self.start}') and
         p.Месяц=month('{self.start}')
@@ -88,7 +145,6 @@ class MainForm:
         """, con=engine_to).merge(sku_df)
         #Пока что заполним пропуски нулями, надо убрать, когда будут полные данные от ГФ
         self.df[['ЗатратыЛогистика', 'ЗатратыСклад']] = self.df[['ЗатратыЛогистика', 'ЗатратыСклад']].fillna(0)
-    
     
     
     def get_recommended_prices(self, no_competitors=False, deficit=False, pickup=False, trades_type=1):
@@ -100,14 +156,16 @@ class MainForm:
         return self.df[['НоменклатураКод', 'Номенклатура', 'УчетнаяЦена', 'РекомендованнаяЦена']], self.df
     
     
-    def get_recommended_price(self, sku, dopobjem=0, artfruit=False, heatseal=False, superpereborka=False):
+    def get_recommended_price(self, sku, toolow=False, toohigh=False, dopobjem=0, artfruit=False, heatseal=False, superpereborka=False):
         return calculate_price(self.df, self.no_competitors, self.deficit, self.pickup, self.trades_type, 
-                               sku=sku, dopobjem=dopobjem, artfruit=artfruit, 
-                               heatseal=heatseal, superpereborka=superpereborka)[['НоменклатураКод', 'Номенклатура', 'УчетнаяЦена', 'РекомендованнаяЦена']]
+                               sku=sku, toolow=toolow, toohigh=toohigh, dopobjem=dopobjem, artfruit=artfruit, 
+                               heatseal=heatseal, superpereborka=superpereborka, toolowvalue=self.toolowvalue,
+                               toohighvalue=self.toohighvalue)[['НоменклатураКод', 'Номенклатура',
+                                                                'УчетнаяЦена', 'РекомендованнаяЦена']]
     
      
 if __name__ == '__main__':
-    mf = MainForm(5527, '2020-04-14', '2020-04-26')
+    mf = MainForm(10763, '2020-04-21', '2020-04-26')
     mf.get_data()
     result, df = mf.get_recommended_prices(no_competitors=False, pickup=True)
-    mf.get_recommended_price(524, dopobjem=600)
+    mf.get_recommended_price(7354, dopobjem=6, toolow=True)
